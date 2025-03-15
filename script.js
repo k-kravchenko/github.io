705aab251f868193b5941b980f2d6ca74d0811d3
k-kravchenko <katerynakravchenko@my.capilanou.ca>
2025-03-15 01:42:48 -0700

    Updated css and html, added js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello, this is my JavaScript file!");
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuToggle.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+});
