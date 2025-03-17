0e416699b08f500e91cfb3e48b0a9912ec3875d0
k-kravchenko <katerynakravchenko@my.capilanou.ca>
2025-03-17 02:25:41 -0700

    Ready to submit

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 console.log("Hello, this is my JavaScript file!");
 document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navLinks = document.querySelector(".nav-links");
+    const menuToggle = document.querySelector(".humburger-icon");
+    const navLinks = document.querySelector(".hb-menu-open");
+    const closeIcon = document.querySelector(".close-icon");
 
     menuToggle.addEventListener("click", function () {
         navLinks.classList.toggle("active");
-    });
-});
+    })
+
+    closeIcon.addEventListener("click", function () {
+        navLinks.classList.remove("active");
+    })
+})
